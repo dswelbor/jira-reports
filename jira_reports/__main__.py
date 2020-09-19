9e36daa23b9b80b30f570f24b93abba98bd6db7a
David Welborn <dswelbor@asu.edu>
2020-09-19 05:10:36 -0400

    Add main to make runnable module

---
+++ jira_reports/__main__.py
@@ -0,0 +1,9 @@
+from jira_reports.hello import main
+
+"""
+This main module serves as an entry point for local development or using
+this package as a runnable module.
+"""
+
+# Entry point for testing
+main()
--- jira_reports/hello.py
+++ jira_reports/hello.py
@@ -2,4 +2,21 @@
 This is a placeholder module. More coming soon.
 """
 
-print('More coming soon. Please don\'t write in the dust')
+
+def pardon_dust():
+    """
+    This is a simple hello world function. More to come.
+    :return: message
+    """
+    msg = 'Hello World. More coming soon. Please don\'t write in the dust.'
+    return msg
+
+
+def main():
+    """
+    This defines the simple main functionality - and serves as the entry
+    point
+    :return: None
+    """
+    str = pardon_dust()
+    print(f'[main]: {str}')
